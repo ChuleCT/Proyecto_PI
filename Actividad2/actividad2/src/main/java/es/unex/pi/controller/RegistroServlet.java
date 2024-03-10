7fd12575324a885f69e06e5ceed98c2148503f3b
Jesús Castaño Tato <91385696+ChuleCT@users.noreply.github.com>
2024-03-10 18:15:07 +0100

    RegistroServlet añadido

---
+++ Actividad2/actividad2/src/main/java/es/unex/pi/controller/RegistroServlet.java
@@ -0,0 +1,69 @@
+package es.unex.pi.controller;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.sql.Connection;
+import java.util.logging.Logger;
+
+import es.unex.pi.dao.JDBCUserDAOImpl;
+import es.unex.pi.dao.UserDAO;
+import es.unex.pi.model.User;
+
+/**
+ * Servlet implementation class RegistroServlet
+ */
+public class RegistroServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	private static final Logger logger = Logger.getLogger(HttpServlet.class.getName());
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public RegistroServlet() {
+        super();
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		
+		logger.info("Atendiendo GET");
+		
+		request.setCharacterEncoding("UTF-8");
+		
+		Connection conn = (Connection) getServletContext().getAttribute("dbConn");
+		UserDAO userDAO = new JDBCUserDAOImpl();
+		userDAO.setConnection(conn);
+		
+		String name = request.getParameter("name");
+		String email = request.getParameter("email");
+		String password = request.getParameter("password");
+		String surname = request.getParameter("surname");
+		Integer id = 3;
+		
+		User user = new User();
+		user.setId(id);
+		user.setName(name);
+		user.setEmail(email);
+		user.setPassword(password);
+		user.setSurname(surname);
+		
+		userDAO.add(user);
+		
+	}
+
+}
